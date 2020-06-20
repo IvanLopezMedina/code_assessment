7fc6d4030a89ebc61f722fe935764145c6b5dd83
Iván López Medina <ivan@Ivans-MBP.home>
2020-06-20 12:31:36 +0200

    test: test client roles

--- clients/clients.test.js
+++ clients/clients.test.js
@@ -4,10 +4,14 @@ const chaiHttp = require('chai-http');
 const assert = require('chai').assert;
 const server = require('../index');
 const {clients} = require('../provider');
+const {user, admin} = require('./clients.roles');
 
 const clientId = 'a0ece5db-cd14-4f21-812f-966633e7be86';
 const clientIdFake = 'c0ece5db-cd14-4f21-812f-966633e7be86';
 const clientName = 'Britney';
+const userHeaders = {role: user};
+const adminHeaders = {role: admin};
+const invalidHeaders = {role: 'invalid_role'};
 
 chai.use(chaiHttp);
 let clientList;
@@ -33,6 +37,7 @@ describe('Clients Unit Test', () => {
       await chai
         .request(server)
         .get('/clients/' + clientId)
+        .set(userHeaders)
         .then(function (res) {
           assert.isTrue(res.body.length === 1);
           assert.strictEqual(res.statusCode, 200);
@@ -42,6 +47,7 @@ describe('Clients Unit Test', () => {
       await chai
         .request(server)
         .get('/clients/' + clientIdFake)
+        .set(userHeaders)
         .then(function (res) {
           assert.isTrue(res.body.length === 0);
           assert.strictEqual(res.statusCode, 200);
@@ -51,9 +57,29 @@ describe('Clients Unit Test', () => {
       await chai
         .request(server)
         .get('/clients/invalid_data')
+        .set(userHeaders)
         .then(function (res) {
           assert.isTrue(res.body.length === 0);
           assert.strictEqual(res.statusCode, 200);
+        });
+    });
+    it('Should return a 403 access denied because there are no role headers', async function () {
+      await chai
+        .request(server)
+        .get('/clients/' + clientId)
+        .then(function (res) {
+          assert.strictEqual(res.text, 'Access denied.');
+          assert.strictEqual(res.statusCode, 403);
+        });
+    });
+    it('Should return a 403 access denied because the role is not admin or user', async function () {
+      await chai
+        .request(server)
+        .get('/clients/' + clientId)
+        .set(invalidHeaders)
+        .then(function (res) {
+          assert.strictEqual(res.text, 'Access denied.');
+          assert.strictEqual(res.statusCode, 403);
         });
     });
   });
@@ -62,6 +88,7 @@ describe('Clients Unit Test', () => {
       await chai
         .request(server)
         .get('/clients/name/' + clientName)
+        .set(adminHeaders)
         .then(function (res) {
           assert.isTrue(res.body.length === 1);
           assert.strictEqual(res.statusCode, 200);
@@ -71,6 +98,7 @@ describe('Clients Unit Test', () => {
       await chai
         .request(server)
         .get('/clients/name/Ivan')
+        .set(adminHeaders)
         .then(function (res) {
           assert.isTrue(res.body.length === 0);
           assert.strictEqual(res.statusCode, 200);
@@ -80,9 +108,29 @@ describe('Clients Unit Test', () => {
       await chai
         .request(server)
         .get('/clients/name/' + clientName[0])
+        .set(adminHeaders)
         .then(function (res) {
           assert.isTrue(res.body.length === 0);
           assert.strictEqual(res.statusCode, 200);
+        });
+    });
+    it('Should return a 403 access denied because there are no role headers', async function () {
+      await chai
+        .request(server)
+        .get('/clients/name/' + clientName)
+        .then(function (res) {
+          assert.strictEqual(res.text, 'Access denied.');
+          assert.strictEqual(res.statusCode, 403);
+        });
+    });
+    it('Should return a 403 access denied because the role is not admin or user', async function () {
+      await chai
+        .request(server)
+        .get('/clients/name/' + clientName)
+        .set(invalidHeaders)
+        .then(function (res) {
+          assert.strictEqual(res.text, 'Access denied.');
+          assert.strictEqual(res.statusCode, 403);
         });
     });
   });
